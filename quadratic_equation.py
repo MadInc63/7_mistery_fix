a374a16b310490b6f9a73074c9bf80993c36c04c
MadInc63 <MadInc63@gmail.com>
2017-11-13 13:21:30 +0400

    так правильнее

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -6,9 +6,9 @@ def get_roots(a, b, c):
     if discriminant >= 0:
         root1 = (-b - sqrt(discriminant)) / (2 * a)
         root2 = (-b + sqrt(discriminant)) / (2 * a)
+    else:
+        return None, None
     if discriminant == 0:
         return root1, None
-    elif discriminant < 0:
-	    return None, None
     else:
         return root1, root2
